ed57ce64e0e28e3d395261926141f2da9f5eab1d
Steve Hocking <steve@codemunki.es>
2017-03-25 22:33:44 +0000

    Add test for two unique items (red)

--- Tests/Checkout.Tests/CheckoutTests.cs
+++ Tests/Checkout.Tests/CheckoutTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using Moq;
 using Moq.AutoMock;
 using Ploeh.AutoFixture;
 using Xunit;
@@ -25,7 +26,7 @@ namespace Checkout.Tests
             var item = AutoFixture.Create<string>();
             var expectedPrice = AutoFixture.Create<int>();
 
-            var calculator =  Mocker.GetMock<ISkuPriceCalculator>();
+            var calculator = Mocker.GetMock<ISkuPriceCalculator>();
             calculator.Setup(calc => calc.TotalPrice()).Returns(expectedPrice);
             Mocker.GetMock<ISkuPriceCalculatorFactory>()
                 .Setup(factory => factory.Build(item))
@@ -37,6 +38,37 @@ namespace Checkout.Tests
 
             // Assert
             result.Should().Be(expectedPrice);
+        }
+
+        [Fact]
+        public void WhenScanningTwoUniqueItemThenThePriceIsTheExpectedPrice()
+        {
+            // Arrange
+            ICheckout subject = Mocker.CreateInstance<Checkout>();
+
+            var item1 = AutoFixture.Create<string>();
+            var expectedPrice1 = AutoFixture.Create<int>();
+            var item2 = AutoFixture.Create<string>();
+            var expectedPrice2 = AutoFixture.Create<int>();
+
+            var calculator1 = new Mock<ISkuPriceCalculator>();
+            calculator1.Setup(calc => calc.TotalPrice()).Returns(expectedPrice1);
+            var calculator2 = new Mock<ISkuPriceCalculator>();
+            calculator2.Setup(calc => calc.TotalPrice()).Returns(expectedPrice2);
+            Mocker.GetMock<ISkuPriceCalculatorFactory>()
+                .Setup(factory => factory.Build(item1))
+                .Returns(calculator1.Object);
+            Mocker.GetMock<ISkuPriceCalculatorFactory>()
+                .Setup(factory => factory.Build(item2))
+                .Returns(calculator2.Object);
+
+            // Act
+            subject.Scan(item1);
+            subject.Scan(item2);
+            var result = subject.GetTotalPrice();
+
+            // Assert
+            result.Should().Be(expectedPrice1 + expectedPrice2);
         }
     }
 
