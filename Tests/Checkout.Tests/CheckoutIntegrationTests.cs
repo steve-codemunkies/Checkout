cc31315b1ee5aef0b0fde77b385a3425d06e87f7
Steve Hocking <steve@codemunki.es>
2017-03-25 22:02:42 +0000

    Move CheckoutIntegrationTests to it's own file

---
+++ Tests/Checkout.Tests/CheckoutIntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Checkout.Tests
+{
+    public class CheckoutIntegrationTests
+    {
+        [Theory]
+        [InlineData("A", 50)]
+        [InlineData("B", 30)]
+        [InlineData("C", 20)]
+        [InlineData("D", 15)]
+        [InlineData("A;a;A", 130)]
+        public void WhenScanningItemsThenTheTotalPriceShouldBeAsExpected(string items, int expectedTotal)
+        {
+            // Arrange
+            ICheckout checkout = new Checkout();
+
+            // Act
+            var itemArray = items.Split(';');
+            foreach (var item in itemArray)
+            {
+                checkout.Scan(item);
+
+            }
+            var result = checkout.GetTotalPrice();
+
+            // Assert
+            result.Should().Be(expectedTotal);
+        }
+    }
+}
