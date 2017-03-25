129e5300d44aa555c83e9cf33e41ad39b717ef50
Steve Hocking <steve@codemunki.es>
2017-03-25 21:01:45 +0000

    Add first test for single item (red)

---
+++ Tests/Checkout.Tests/CheckoutTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Checkout.Tests
+{
+    public class CheckoutTests
+    {
+        [Fact]
+        public void WhenScanningOneItemAThenTheTotalPriceShouldBeFifty()
+        {
+            // Arrange
+            ICheckout checkout = new Checkout();
+
+            // Act
+            checkout.Scan("A");
+            var result = checkout.GetTotalPrice();
+
+            // Assert
+            result.Should().Be(50);
+        }
+    }
+}
