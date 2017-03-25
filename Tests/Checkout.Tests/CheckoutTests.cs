2e8c1dff012408904d6f2cc1edfd14403ddde38e
Steve Hocking <steve@codemunki.es>
2017-03-25 21:56:31 +0000

    Add new theory for three item A's (red)

--- Tests/Checkout.Tests/CheckoutTests.cs
+++ Tests/Checkout.Tests/CheckoutTests.cs
@@ -10,13 +10,19 @@ namespace Checkout.Tests
         [InlineData("B", 30)]
         [InlineData("C", 20)]
         [InlineData("D", 15)]
-        public void WhenScanningAnItemThenTheTotalPriceShouldBeAsExpected(string item, int expectedTotal)
+        [InlineData("A;a;A", 130)]
+        public void WhenScanningItemsThenTheTotalPriceShouldBeAsExpected(string items, int expectedTotal)
         {
             // Arrange
             ICheckout checkout = new Checkout();
 
             // Act
-            checkout.Scan(item);
+            var itemArray = items.Split(';');
+            foreach (var item in itemArray)
+            {
+                checkout.Scan(item);
+
+            }
             var result = checkout.GetTotalPrice();
 
             // Assert
