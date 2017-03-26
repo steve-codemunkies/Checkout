18b925419d4dd041ef20e02f628e3d7e8261a455
Steve Hocking <steve@codemunki.es>
2017-03-26 23:59:55 +0100

    Add ConventionRegistration module
    Registers everything in the Checkout assembly.

---
+++ src/Checkout/Ioc/ConventionModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+
+namespace Checkout.Ioc
+{
+    public class ConventionModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(GetType().Assembly)
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
