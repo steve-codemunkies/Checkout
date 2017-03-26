a00d9b52b73424281b8608589a58c00045e99cd7
Steve Hocking <steve@codemunki.es>
2017-03-27 00:11:38 +0100

    Setup autofac in integration test (red)

--- Tests/Checkout.Tests/CheckoutIntegrationTests.cs
+++ Tests/Checkout.Tests/CheckoutIntegrationTests.cs
@@ -1,11 +1,26 @@
-﻿using Checkout.Interfaces;
+﻿using System;
+using System.Configuration;
+using Autofac;
+using Checkout.Interfaces;
+using Checkout.Ioc;
 using FluentAssertions;
 using Xunit;
 
 namespace Checkout.Tests
 {
-    public class CheckoutIntegrationTests
+    public class CheckoutIntegrationTests : IDisposable
     {
+        private IContainer _container;
+
+        public CheckoutIntegrationTests()
+        {
+            var connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new DatabaseModule(connectionString));
+            builder.RegisterModule<ConventionModule>();
+            _container = builder.Build();
+        }
+
         [Theory]
         [InlineData("A", 50)]
         [InlineData("B", 30)]
@@ -14,20 +29,29 @@ namespace Checkout.Tests
         [InlineData("A;a;A", 130)]
         public void WhenScanningItemsThenTheTotalPriceShouldBeAsExpected(string items, int expectedTotal)
         {
-            // Arrange
-            ICheckout checkout = null; //new Checkout();
-
-            // Act
-            var itemArray = items.Split(';');
-            foreach (var item in itemArray)
+            using (var innerScope = _container.BeginLifetimeScope())
             {
-                checkout.Scan(item);
+                // Arrange
+                ICheckout checkout = innerScope.Resolve<ICheckout>();
 
+                // Act
+                var itemArray = items.Split(';');
+                foreach (var item in itemArray)
+                {
+                    checkout.Scan(item);
+
+                }
+                var result = checkout.GetTotalPrice();
+
+                // Assert
+                result.Should().Be(expectedTotal);
             }
-            var result = checkout.GetTotalPrice();
+        }
 
-            // Assert
-            result.Should().Be(expectedTotal);
+        public void Dispose()
+        {
+            _container.Dispose();
+            _container = null;
         }
     }
 }
