9dade9888f293b9219fd684ad3b081be53ca909c
Steve Hocking <steve@codemunki.es>
2017-03-26 22:11:10 +0100

    Refactor tests to use inline data (green)
    Refactored tests to use less code.

--- Tests/Checkout.Tests/SkuPriceCalculatorTests.cs
+++ Tests/Checkout.Tests/SkuPriceCalculatorTests.cs
@@ -57,109 +57,40 @@ namespace Checkout.Tests
             subject.IsCalculatingPriceForItem(item.ToLowerInvariant()).Should().Be(true);
         }
 
-        [Fact]
-        public void WhenInitiallyNewedTheCalculatorReturnsTheExpectedPrice()
+        [Theory]
+        [InlineData("A", 50, 3, 130, 1, 50)]
+        [InlineData("A", 50, 3, 130, 2, 100)]
+        [InlineData("A", 50, 3, 130, 3, 130)]
+        [InlineData("A", 50, 3, 130, 4, 180)]
+        [InlineData("A", 50, 3, 130, 6, 260)]
+        [InlineData("A", 50, 3, 130, 7, 310)]
+        [InlineData("B", 30, 2, 45, 1, 30)]
+        [InlineData("B", 30, 2, 45, 2, 45)]
+        [InlineData("B", 30, 2, 45, 3, 75)]
+        [InlineData("B", 30, 2, 45, 4, 90)]
+        [InlineData("B", 30, 2, 45, 5, 120)]
+        [InlineData("C", 20, null, null, 1, 20)]
+        [InlineData("C", 20, null, null, 2, 40)]
+        [InlineData("C", 20, null, null, 4, 80)]
+        [InlineData("C", 20, null, null, 8, 160)]
+        [InlineData("D", 15, null, null, 1, 15)]
+        [InlineData("D", 15, null, null, 2, 30)]
+        [InlineData("D", 15, null, null, 3, 45)]
+        [InlineData("D", 15, null, null, 5, 75)]
+        public void WhenCalculatingThePriceOfItems(string item, int unitPrice, int? multiBuyItemCount,
+            int? multiBuyPrice, int count, int expectedPrice)
         {
             // Arrange
-            var item = AutoFixture.Create<string>();
-            var expectedPrice = AutoFixture.Create<int>();
-            var subject = new SkuPriceCalculator(item, expectedPrice);
+            ISkuPriceCalculator subject = new SkuPriceCalculator(item, unitPrice, multiBuyItemCount, multiBuyPrice);
 
             // Act
-            // Assert
-            subject.TotalPrice().Should().Be(expectedPrice);
-        }
-
-        [Fact]
-        public void WhenCalculatingThePriceForTwoItemsItShouldReturnTheCorrectPrice()
-        {
-            // Arrange
-            var item = AutoFixture.Create<string>();
-            var expectedPrice = AutoFixture.Create<int>();
-            var subject = new SkuPriceCalculator(item, expectedPrice);
-
-            // Act
-            subject.IncrementItemCount();
-
-            // Assert
-            subject.TotalPrice().Should().Be(expectedPrice * 2);
-        }
-
-        [Fact]
-        public void WhenCalculatingThePriceForNItemsItShouldReturnTheCorrectPrice()
-        {
-            // Arrange
-            var item = AutoFixture.Create<string>();
-            var expectedPrice = AutoFixture.Create<int>();
-            var itemCount = Random.Next(1, 11);
-            var subject = new SkuPriceCalculator(item, expectedPrice);
-
-            // Act
-            for (var i = 1; i < itemCount; i++)
+            for (int i = 1; i < count; i++)
             {
                 subject.IncrementItemCount();
             }
 
             // Assert
-            subject.TotalPrice().Should().Be(expectedPrice * itemCount);
-        }
-
-        [Fact]
-        public void WhenCalculatingThePriceOf3ItemsWithAMultiBuySpecialOffer()
-        {
-            // Arrange
-            var item = AutoFixture.Create<string>();
-            var singlePrice = 50;
-            var multiBuyItemCount = 3;
-            var multiBuyPrice = 130;
-            var subject = new SkuPriceCalculator(item, singlePrice, multiBuyItemCount, multiBuyPrice);
-
-            // Act
-            subject.IncrementItemCount();
-            subject.IncrementItemCount();
-
-            // Assert
-            subject.TotalPrice().Should().Be(multiBuyPrice);
-        }
-
-        [Fact]
-        public void WhenCalculatingThePriceOf4ItemsWithAMultiBuySpecialOffer()
-        {
-            // Arrange
-            var item = AutoFixture.Create<string>();
-            var singlePrice = 50;
-            var multiBuyItemCount = 3;
-            var multiBuyPrice = 130;
-            var subject = new SkuPriceCalculator(item, singlePrice, multiBuyItemCount, multiBuyPrice);
-
-            // Act
-            subject.IncrementItemCount();
-            subject.IncrementItemCount();
-            subject.IncrementItemCount();
-
-            // Assert
-            subject.TotalPrice().Should().Be(singlePrice + multiBuyPrice);
-        }
-
-        [Fact]
-        public void WhenCalculatingThePriceOf6ItemsWithAMultiBuySpecialOffer()
-        {
-            // Arrange
-            var item = AutoFixture.Create<string>();
-            var singlePrice = 50;
-            var multiBuyItemCount = 3;
-            var multiBuyPrice = 130;
-            var subject = new SkuPriceCalculator(item, singlePrice, multiBuyItemCount, multiBuyPrice);
-
-            // Act
-            subject.IncrementItemCount();
-            subject.IncrementItemCount();
-            subject.IncrementItemCount();
-            subject.IncrementItemCount();
-            subject.IncrementItemCount();
-
-            // Assert
-            subject.TotalPrice().Should().Be(multiBuyPrice * 2);
+            subject.TotalPrice().Should().Be(expectedPrice);
         }
     }
 
