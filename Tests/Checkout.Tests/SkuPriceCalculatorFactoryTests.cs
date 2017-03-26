552bbf156ba7fc05b792116f47ac65516aa4ef26
Steve Hocking <steve@codemunki.es>
2017-03-26 22:51:14 +0100

    Add implementation of method
    Additionally added:
    
    * SkuWithMultiBuy type to hold configuration data
    * IGetSkuWithMultiBuy to query for config

--- Tests/Checkout.Tests/SkuPriceCalculatorFactoryTests.cs
+++ Tests/Checkout.Tests/SkuPriceCalculatorFactoryTests.cs
@@ -1,16 +1,32 @@
 ﻿using Checkout.Interfaces;
 using FluentAssertions;
+using Moq.AutoMock;
 using Xunit;
 
 namespace Checkout.Tests
 {
     public class SkuPriceCalculatorFactoryTests
     {
+        public AutoMocker Mocker { get; set; }
+
+        public SkuPriceCalculatorFactoryTests()
+        {
+            Mocker = new AutoMocker();
+        }
+
         [Fact]
         public void WhenBuildSkuPriceCalculatorACorrectlyPopulatedInstanceIsReturned()
         {
             // Arrange
-            ISkuPriceCalculatorFactory subject = new SkuPriceCalculatorFactory();
+            var skuWithMultiBuy = new SkuWithMultiBuy
+            {
+                Item = "A",
+                UnitPrice = 50,
+                MultiBuyItemCount = 3,
+                MultiBuyPrice = 130
+            };
+            ISkuPriceCalculatorFactory subject = Mocker.CreateInstance<SkuPriceCalculatorFactory>();
+            Mocker.GetMock<IGetSkuWithMultiBuy>().Setup(get => get.Query(skuWithMultiBuy.Item)).Returns(skuWithMultiBuy);
 
             // Act
             var result = subject.Build("A");
@@ -25,11 +41,33 @@ namespace Checkout.Tests
         }
     }
 
+    public interface IGetSkuWithMultiBuy
+    {
+        SkuWithMultiBuy Query(string item);
+    }
+
+    public class SkuWithMultiBuy
+    {
+        public string Item { get; set; }
+        public int UnitPrice { get; set; }
+        public int MultiBuyItemCount { get; set; }
+        public int MultiBuyPrice { get; set; }
+    }
+
     public class SkuPriceCalculatorFactory : ISkuPriceCalculatorFactory
     {
+        private readonly IGetSkuWithMultiBuy _getSkuWithMultiBuy;
+
+        public SkuPriceCalculatorFactory(IGetSkuWithMultiBuy getSkuWithMultiBuy)
+        {
+            _getSkuWithMultiBuy = getSkuWithMultiBuy;
+        }
+
         public ISkuPriceCalculator Build(string item)
         {
-            throw new System.NotImplementedException();
+            var skuWithMultiBuy = _getSkuWithMultiBuy.Query(item);
+            return new SkuPriceCalculator(skuWithMultiBuy.Item, skuWithMultiBuy.UnitPrice,
+                skuWithMultiBuy.MultiBuyItemCount, skuWithMultiBuy.MultiBuyPrice);
         }
     }
 }
