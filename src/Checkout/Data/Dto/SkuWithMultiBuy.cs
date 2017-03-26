2ddd72e1783fc030b0845b7bda33f9af40bc8fa4
Steve Hocking <steve@codemunki.es>
2017-03-26 23:52:37 +0100

    Update SkuWithMultiBuy to use as nhibernate dto

--- src/Checkout/Data/Dto/SkuWithMultiBuy.cs
+++ src/Checkout/Data/Dto/SkuWithMultiBuy.cs
@@ -1,10 +1,27 @@
+using FluentNHibernate.Mapping;
+
 namespace Checkout.Data.Dto
 {
     public class SkuWithMultiBuy
     {
-        public string Item { get; set; }
-        public int UnitPrice { get; set; }
-        public int? MultiBuyItemCount { get; set; }
-        public int? MultiBuyPrice { get; set; }
+        public virtual int Id { get; set; }
+        public virtual string Item { get; set; }
+        public virtual int UnitPrice { get; set; }
+        public virtual int? MultiBuyItemCount { get; set; }
+        public virtual int? MultiBuyPrice { get; set; }
+
+        public class SkuWithMultiBuyMap : ClassMap<SkuWithMultiBuy>
+        {
+            public SkuWithMultiBuyMap()
+            {
+                Schema("dbo");
+                Table("SkuWithMultiBuy");
+                Id(dto => dto.Id);
+                Map(dto => dto.Item);
+                Map(dto => dto.UnitPrice);
+                Map(dto => dto.MultiBuyItemCount);
+                Map(dto => dto.MultiBuyPrice);
+            }
+        }
     }
 }
