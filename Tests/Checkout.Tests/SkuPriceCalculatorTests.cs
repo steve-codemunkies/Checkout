8e6a2f638c021b96944fc8017bb24d681f8fd7df
Steve Hocking <steve@codemunki.es>
2017-03-26 20:08:41 +0100

    Add test class and first test for SkuPriceCalculator (red)

---
+++ Tests/Checkout.Tests/SkuPriceCalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using Checkout.Interfaces;
+using FluentAssertions;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace Checkout.Tests
+{
+    public class SkuPriceCalculatorTests
+    {
+        public Fixture AutoFixture { get; set; }
+
+        public SkuPriceCalculatorTests()
+        {
+            AutoFixture = new Fixture();
+        }
+
+        [Fact]
+        public void WhenCheckingIsCalculatingPriceForItem()
+        {
+            // Arrange
+            var item = AutoFixture.Create<string>();
+            ISkuPriceCalculator subject = new SkuPriceCalculator();
+
+            // Act
+            // Assert
+            subject.IsCalculatingPriceForItem(item).Should().Be(true);
+        }
+    }
+}
