3805d2bb5f028f945b6d16cc0d96c9e49faa8e6c
Steve Hocking <steve@codemunki.es>
2017-03-26 23:29:44 +0100

    Add SkuWithMultiBuy table definition

---
+++ Database/Checkout.Database/dbo/Tables/SkuWithMultiBuy.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SkuWithMultiBuy]
+(
+	[Id] INT NOT NULL IDENTITY(1, 1) PRIMARY KEY, 
+    [Item] NVARCHAR(13) NOT NULL, 
+    [UnitPrice] INT NOT NULL, 
+    [MultiBuyItemCount] INT NULL, 
+    [MultiBuyPrice] INT NULL
+)
