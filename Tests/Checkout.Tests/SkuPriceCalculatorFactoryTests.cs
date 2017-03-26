46486700d025bab1438171cdb42d13f880b55794
Steve Hocking <steve@codemunki.es>
2017-03-26 22:31:21 +0100

    Add test for correctly configged calc (red)

---
+++ Tests/Checkout.Tests/SkuPriceCalculatorFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using Checkout.Interfaces;
+using FluentAssertions;
+using Xunit;
+
+namespace Checkout.Tests
+{
+    public class SkuPriceCalculatorFactoryTests
+    {
+        [Fact]
+        public void WhenBuildSkuPriceCalculatorACorrectlyPopulatedInstanceIsReturned()
+        {
+            // Arrange
+            ISkuPriceCalculatorFactory subject = new SkuPriceCalculatorFactory();
+
+            // Act
+            var result = subject.Build("A");
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<SkuPriceCalculator>();
+            result.TotalPrice().Should().Be(50);
+            result.IncrementItemCount();
+            result.IncrementItemCount();
+            result.TotalPrice().Should().Be(130);
+        }
+    }
+}
